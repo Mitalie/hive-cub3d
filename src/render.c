f1cf5458e66fb76a97140089bf18a6d32abfebd8
theDapperFoxtrot <thedapperfoxtrot.bizinq@gmail.com>
2025-07-04 17:21:06 +0300

    Avoid using doubles instead of floats in render and cast

--- src/render.c
+++ src/render.c
@@ -33,14 +33,14 @@ static void	render_column(t_cub3d *cub3d, float tan_vfov, int col, t_hit *hit)
 	float		tan_wall_height;
 	uint32_t	color;
 
-	tan_wall_height = 0.5 / hit->distance;
+	tan_wall_height = 0.5f / hit->distance;
 	row = 0;
 	while (row < cub3d->height)
 	{
-		tan_vert = -1 * tan_vfov * ((row + 0.5) / cub3d->height - 0.5);
+		tan_vert = -1 * tan_vfov * ((row + 0.5f) / cub3d->height - 0.5f);
 		if (tan_vert > -tan_wall_height && tan_vert < tan_wall_height)
 			color = wall_color(hit->side, hit->position_in_tile,
-					tan_vert / tan_wall_height * 0.5 + 0.5);
+					tan_vert / tan_wall_height * 0.5f + 0.5f);
 		else if (tan_vert > 0)
 			color = 0xffffff;
 		else
@@ -50,6 +50,8 @@ static void	render_column(t_cub3d *cub3d, float tan_vfov, int col, t_hit *hit)
 	}
 }
 
+#define DEG_TO_RAD 0.017453292519943295769f
+
 void	render_view(t_cub3d *cub3d)
 {
 	float	tan_hfov;
@@ -58,13 +60,13 @@ void	render_view(t_cub3d *cub3d)
 	int		col;
 	t_hit	hit;
 
-	tan_hfov = 2 * tan(cub3d->hfov_deg / 2 * 3.1415926535 / 180);
+	tan_hfov = 2 * tanf(cub3d->hfov_deg * 0.5f * DEG_TO_RAD);
 	tan_vfov = tan_hfov * cub3d->height / cub3d->width;
 	col = 0;
 	while (col < cub3d->width)
 	{
 		direction.y = -1;
-		direction.x = tan_hfov * ((col + 0.5) / cub3d->width - 0.5);
+		direction.x = tan_hfov * ((col + 0.5f) / cub3d->width - 0.5f);
 		direction = vec2_rotate(direction, cub3d->player_facing);
 		cast(cub3d, cub3d->player, direction, &hit);
 		render_column(cub3d, tan_vfov, col, &hit);
