620f75f5a7fbd1420b4a91b2619cb75efc15ad38
theDapperFoxtrot <thedapperfoxtrot.bizinq@gmail.com>
2025-08-20 16:03:47 +0300

    Refactor cast_door
    * Drop green/red/yellow naming, call it left/right half of face, and
      edge.
    * Remove animation comment that doesn't belong here anymore.
    * Move door face hit in a separate function.
    * Split N/S and E/W door edge hit into separate functions.

--- src/cast/cast_door.c
+++ src/cast/cast_door.c
@@ -7,81 +7,75 @@
 #include "cub3d.h"
 #include "map.h"
 
-/*
-	`map_door` should never return `NULL`, because we only call `cast_door` if
-	`map_tile` returned `TILE_DOOR_NS` or `TILE_DOOR_EW`. If this happens, the
-	map parsing is broken, but we just assume it is not.
-
-	time since anim start 0.0-0.5 -> state 0.0-1.0
-	time since anim start 0.5-3.5 -> state 1.0
-	time since anim start 3.5-4.0 -> state 1.0-0.0
-	time since anim start 4.0-inf -> state 0.0
-
-	red = [0 * (1 - state), 0.5 * (1 - state)]
-	green = [0.5 * (1 + state), 1]
-*/
-
-static bool	cast_door_yellow(t_cast_state *state, float door_state)
+static bool	cast_door_edge_ns(t_cast_state *state, t_hit *hit, float door_state)
 {
-	float			yellow_line_pos;
+	float			edge_x;
 	t_intersection	intersection;
-	t_hit			*hit;
 
-	hit = &state->cr->opaque;
-	if (hit->side == HIT_NORTH || hit->side == HIT_SOUTH)
-	{
-		yellow_line_pos = state->tile_x + 0.5f + copysignf(0.5f * door_state, state->dir.x);
-		intersection = intersect_y(state->pos, state->dir, yellow_line_pos);
-		if (isinff(intersection.ray_len) || floorf(intersection.tgt_pos) != state->tile_y)
-			return (false);
-		hit->distance = intersection.ray_len;
-		hit->pos_in_tile = intersection.tgt_pos - floorf(intersection.tgt_pos);
-		hit->material = MAT_DOOR_SIDE;
-		return (true);
-	}
+	edge_x = state->tile_x + 0.5f + copysignf(0.5f * door_state, state->dir.x);
+	intersection = intersect_y(state->pos, state->dir, edge_x);
+	if (isinff(intersection.ray_len)
+		|| floorf(intersection.tgt_pos) != state->tile_y)
+		return (false);
+	hit->distance = intersection.ray_len;
+	hit->pos_in_tile = intersection.tgt_pos - floorf(intersection.tgt_pos);
+	hit->material = MAT_DOOR_SIDE;
+	return (true);
+}
+
+static bool	cast_door_edge_ew(t_cast_state *state, t_hit *hit, float door_state)
+{
+	float			edge_y;
+	t_intersection	intersection;
+
+	edge_y = state->tile_y + 0.5f + copysignf(0.5f * door_state, state->dir.y);
+	intersection = intersect_x(state->pos, state->dir, edge_y);
+	if (isinff(intersection.ray_len)
+		|| floorf(intersection.tgt_pos) != state->tile_x)
+		return (false);
+	hit->distance = intersection.ray_len;
+	hit->pos_in_tile = intersection.tgt_pos - floorf(intersection.tgt_pos);
+	hit->material = MAT_DOOR_SIDE;
+	return (true);
+}
+
+static bool	cast_door_face(t_hit *hit, float door_state,
+	t_intersection intersection)
+{
+	float	pos_on_face;
+	float	pos_on_left_half;
+	float	pos_on_right_half;
+
+	pos_on_face = intersection.tgt_pos;
+	if (hit->side == HIT_NORTH || hit->side == HIT_EAST)
+		pos_on_face = ceilf(pos_on_face) - pos_on_face;
 	else
-	{
-		yellow_line_pos = state->tile_y + 0.5f + copysignf(0.5f * door_state, state->dir.y);
-		intersection = intersect_x(state->pos, state->dir, yellow_line_pos);
-		if (isinff(intersection.ray_len) || floorf(intersection.tgt_pos) != state->tile_x)
-			return (false);
-		hit->distance = intersection.ray_len;
-		hit->pos_in_tile = intersection.tgt_pos - floorf(intersection.tgt_pos);
-		hit->material = MAT_DOOR_SIDE;
-		return (true);
-	}
+		pos_on_face = pos_on_face - floorf(pos_on_face);
+	pos_on_left_half = pos_on_face + 0.5f * door_state;
+	pos_on_right_half = pos_on_face - 0.5f * door_state;
+	if (pos_on_left_half < 0.5f)
+		hit->pos_in_tile = pos_on_left_half;
+	else if (pos_on_right_half >= 0.5f)
+		hit->pos_in_tile = pos_on_right_half;
+	else
+		return (false);
+	hit->distance = intersection.ray_len;
+	hit->material = MAT_DOOR_FACE;
+	return (true);
 }
 
 bool	cast_door(t_cast_state *state, t_intersection intersection)
 {
-	float	door_state;
-	float	front_pos;
-	float	red_pos;
-	float	green_pos;
 	t_hit	*hit;
+	float	door_state;
 
 	hit = &state->cr->opaque;
-	door_state = map_door_state(&state->cub3d->map, state->tile_x, state->tile_y);
-	front_pos = intersection.tgt_pos;
-	if (hit->side == HIT_NORTH || hit->side == HIT_EAST)
-		front_pos = ceilf(front_pos) - front_pos;
+	door_state = map_door_state(
+			&state->cub3d->map, state->tile_x, state->tile_y);
+	if (cast_door_face(hit, door_state, intersection))
+		return (true);
+	if (hit->side == HIT_NORTH || hit->side == HIT_SOUTH)
+		return (cast_door_edge_ns(state, hit, door_state));
 	else
-		front_pos = front_pos - floorf(front_pos);
-	red_pos = front_pos + 0.5f * door_state;
-	green_pos = front_pos - 0.5f * door_state;
-	if (red_pos <= 0.5f)
-	{
-		hit->distance = intersection.ray_len;
-		hit->pos_in_tile = red_pos;
-		hit->material = MAT_DOOR_FACE;
-		return (true);
-	}
-	if (green_pos >= 0.5f)
-	{
-		hit->distance = intersection.ray_len;
-		hit->pos_in_tile = green_pos;
-		hit->material = MAT_DOOR_FACE;
-		return (true);
-	}
-	return (cast_door_yellow(state, door_state));
+		return (cast_door_edge_ew(state, hit, door_state));
 }
